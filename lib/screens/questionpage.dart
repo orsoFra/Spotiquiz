27df0a89c5618e1fe2130e3aba6b06c68c28e4f4
giulis13 <giulis13@hotmail.it>
2022-12-26 16:53:33 +0100

    Fix graphics overflow in questionPage

--- lib/screens/questionpage.dart
+++ lib/screens/questionpage.dart
@@ -55,107 +55,110 @@ class _QuestionPageState extends State<QuestionPage> {
           backgroundColor: Colors.teal,
           elevation: 0,
         ),
-        body: Column(
-          children: [
-            FutureBuilder<QuestionModel>(
-                future: qApi.generateRandomQuestion(http.Client()),
-                builder: (BuildContext context, AsyncSnapshot<QuestionModel> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                      // TODO: mettere widget che fa loading invece del text
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Center(child: const CircularProgressIndicator()),
-                        ],
-                      );
-                    case ConnectionState.done:
-                      if (snapshot.data == null) print('data is null');
-                      return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            "Question 1/1",
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w300,
-                              fontSize: 28.0,
-                            ),
-                          ),
-                        ),
-                        // TODO: change it when you will have more questions
-
-                        Divider(
-                          color: Colors.white,
-                          height: 8.0,
-                          thickness: 1.0,
-                        ),
-
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-
-                        SizedBox(
-                          width: double.infinity,
-                          height: 200.0,
-                          child: Text(
-                            snapshot.data!.question,
-                            style: TextStyle(color: Colors.white, fontSize: 22.0),
-                          ),
-                        ),
-                        if (snapshot.data!.songURL != null)
-                          ValueListenableBuilder(
-                              valueListenable: isPlaying,
-                              builder: (context, value, child) => IconButton(
-                                  iconSize: 50,
-                                  icon: (isPlaying.value) ? const Icon(Icons.stop) : Icon(Icons.play_arrow),
-                                  onPressed: () {
-                                    isPlaying.value = !isPlaying.value;
-                                    playPauseAudioNetwork(snapshot.data!.songURL!, isPlaying.value);
-                                  })),
-
-                        SizedBox(
-                          height: 25.0,
-                        ),
-
-                        // generate 4 bottons for the answers
-                        for (int i = 0; i < snapshot.data!.answers.length; i++)
-                          Container(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              FutureBuilder<QuestionModel>(
+                  future: qApi.generateRandomQuestion(http.Client()),
+                  builder: (BuildContext context, AsyncSnapshot<QuestionModel> snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                        // TODO: mettere widget che fa loading invece del text
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Center(child: const CircularProgressIndicator()),
+                          ],
+                        );
+                      case ConnectionState.done:
+                        if (snapshot.data == null) print('data is null');
+                        return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+                          SizedBox(
                             width: double.infinity,
-                            margin: EdgeInsets.only(bottom: 18.0),
-                            child: MaterialButton(
-                              shape: StadiumBorder(),
-                              color: Colors.blueAccent,
-                              padding: EdgeInsets.symmetric(vertical: 18.0),
-                              onPressed: () {},
-                              child: Text(snapshot.data!.answers[i], style: TextStyle(color: Colors.white)),
-                            ),
-                          ),
-                        SizedBox(
-                          height: 30.0,
-                        ),
-                        OutlinedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                              // we will do it later
+                            child: Text(
+                              "Question 1/1",
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                                fontSize: 28.0,
                               ),
-                          child: Text(
-                            "Next Question",
-                            style: TextStyle(
-                              color: Colors.white,
                             ),
                           ),
-                        )
-                      ]);
-                    default:
-                      if (snapshot.hasError)
-                        return new Text('Error: ${snapshot.error}');
-                      else
-                        return new Text('Result: ${snapshot.data}');
-                  }
-                }),
-          ],
+                          // TODO: change it when you will have more questions
+
+                          Divider(
+                            color: Colors.white,
+                            height: 8.0,
+                            thickness: 1.0,
+                          ),
+
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+
+                          SizedBox(
+                            width: double.infinity,
+                            height: 200.0,
+                            child: Text(
+                              snapshot.data!.question,
+                              style: TextStyle(color: Colors.white, fontSize: 22.0),
+                            ),
+                          ),
+                          if (snapshot.data!.songURL != null)
+                            ValueListenableBuilder(
+                                valueListenable: isPlaying,
+                                builder: (context, value, child) => IconButton(
+                                    iconSize: 50,
+                                    icon: (isPlaying.value) ? const Icon(Icons.stop) : Icon(Icons.play_arrow),
+                                    onPressed: () {
+                                      isPlaying.value = !isPlaying.value;
+                                      playPauseAudioNetwork(snapshot.data!.songURL!, isPlaying.value);
+                                    })),
+
+                          SizedBox(
+                            height: 25.0,
+                          ),
+
+                          // generate 4 bottons for the answers
+                          for (int i = 0; i < snapshot.data!.answers.length; i++)
+                            Container(
+                              width: double.infinity,
+                              margin: EdgeInsets.only(bottom: 18.0),
+                              child: MaterialButton(
+                                shape: StadiumBorder(),
+                                color: Colors.blueAccent,
+                                padding: EdgeInsets.symmetric(vertical: 18.0),
+                                onPressed: () {},
+                                child: Text(snapshot.data!.answers[i], style: TextStyle(color: Colors.white)),
+                              ),
+                            ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          // final button to go to the next question --> delete it
+                          OutlinedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                                // we will do it later
+                                ),
+                            child: Text(
+                              "Next Question",
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          )
+                        ]);
+                      default:
+                        if (snapshot.hasError)
+                          return new Text('Error: ${snapshot.error}');
+                        else
+                          return new Text('Result: ${snapshot.data}');
+                    }
+                  }),
+            ],
+          ),
         ));
   }
 }
