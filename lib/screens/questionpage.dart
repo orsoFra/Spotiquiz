187fd94bc85c263aab7c38bcc7d70c93a3eff095
giulis13 <giulis13@hotmail.it>
2022-12-27 19:22:34 +0100

    Changing question done with green or red buttons.

--- lib/screens/questionpage.dart
+++ lib/screens/questionpage.dart
@@ -22,7 +22,8 @@ void playPauseAudioNetwork(String URL, bool doPlay) async {
     assetsAudioPlayer.pause();
   } else {
     try {
-      await assetsAudioPlayer.open(Audio.network(URL), autoStart: false, showNotification: true);
+      await assetsAudioPlayer.open(Audio.network(URL),
+          autoStart: false, showNotification: true);
       assetsAudioPlayer.play();
     } catch (t) {
       print(t);
@@ -39,18 +40,20 @@ class QuestionPage extends StatefulWidget {
 
 class _QuestionPageState extends State<QuestionPage> {
   ValueNotifier<bool> isPlaying = ValueNotifier(false);
-  late List<Future<QuestionModel >> questions;
+  List<Future<QuestionModel>>? questions;
+
   //int question_pos = 0;
   //int score = 0;
   //bool btnPressed = false;
-  //PageController? _controller;
+  PageController? _controller;
+
   //bool answered  = false;
 
   @override
   void initState() {
     isPlaying = ValueNotifier<bool>(false);
     super.initState();
-    //_controller = PageController(initialPage: 0);
+    _controller = PageController(initialPage: 0);
     this.questions = qApi.generateRandomQuestions(http.Client(), 10);
   }
 
@@ -63,110 +66,170 @@ class _QuestionPageState extends State<QuestionPage> {
           backgroundColor: Colors.teal,
           elevation: 0,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              FutureBuilder<QuestionModel>(
-                  future: this.questions[0],
-                  builder: (BuildContext context, AsyncSnapshot<QuestionModel> snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        // TODO: mettere widget che fa loading invece del text
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Center(child: const CircularProgressIndicator()),
-                          ],
-                        );
-                      case ConnectionState.done:
-                        if (snapshot.data == null) print('data is null');
-                        return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
-                          SizedBox(
-                            width: double.infinity,
-                            child: Text(
-                              "Question 1/1",
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w300,
-                                fontSize: 28.0,
-                              ),
-                            ),
-                          ),
-                          // TODO: change it when you will have more questions
-
-                          Divider(
-                            color: Colors.white,
-                            height: 8.0,
-                            thickness: 1.0,
-                          ),
-
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-
-                          SizedBox(
-                            width: double.infinity,
-                            height: 200.0,
-                            child: Text(
-                              snapshot.data!.question,
-                              style: TextStyle(color: Colors.white, fontSize: 22.0),
-                            ),
-                          ),
-                          if (snapshot.data!.songURL != null)
-                            ValueListenableBuilder(
-                                valueListenable: isPlaying,
-                                builder: (context, value, child) => IconButton(
-                                    iconSize: 50,
-                                    icon: (isPlaying.value) ? const Icon(Icons.stop) : Icon(Icons.play_arrow),
-                                    onPressed: () {
-                                      isPlaying.value = !isPlaying.value;
-                                      playPauseAudioNetwork(snapshot.data!.songURL!, isPlaying.value);
-                                    })),
-
-                          SizedBox(
-                            height: 25.0,
-                          ),
-
-                          // generate 4 bottons for the answers
-                          for (int i = 0; i < snapshot.data!.answers.length; i++)
-                            Container(
-                              width: double.infinity,
-                              margin: EdgeInsets.only(bottom: 18.0),
-                              child: MaterialButton(
-                                shape: StadiumBorder(),
-                                color: Colors.blueAccent,
-                                padding: EdgeInsets.symmetric(vertical: 18.0),
-                                onPressed: () {},
-                                child: Text(snapshot.data!.answers[i], style: TextStyle(color: Colors.white)),
-                              ),
-                            ),
-                          SizedBox(
-                            height: 30.0,
-                          ),
-                          // final button to go to the next question --> delete it
-                          OutlinedButton(
-                            onPressed: () {},
-                            style: ButtonStyle(
-                                // we will do it later
-                                ),
-                            child: Text(
-                              "Next Question",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          )
-                        ]);
-                      default:
-                        if (snapshot.hasError)
-                          return new Text('Error: ${snapshot.error}');
-                        else
-                          return new Text('Result: ${snapshot.data}');
+        body: PageView.builder(
+                  itemCount: questions!.length,
+                  controller: _controller!,
+                  onPageChanged: (page) {
+                    if (page == questions!.length - 1) {
+                      setState(() {
+                        //btnText = "See Results";
+                        print("finish");
+                      });
                     }
+                    setState(() {
+                      //answered = false;
+                      print("page changed");
+                    });
+                  },
+                  physics: new NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          FutureBuilder<QuestionModel>(
+                              future: this.questions![index],
+                              builder: (BuildContext context,
+                                  AsyncSnapshot<QuestionModel> snapshot) {
+                                switch (snapshot.connectionState) {
+                                  case ConnectionState.waiting:
+                                    // TODO: mettere widget che fa loading invece del text
+                                    return Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Center(
+                                            child: const CircularProgressIndicator()),
+                                      ],
+                                    );
+                                  case ConnectionState.done:
+                                    if (snapshot.data == null) print('data is null');
+                                    return Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          SizedBox(
+                                            width: double.infinity,
+                                            child: Text(
+                                              "Question ${index+1}/${questions!.length}",
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w300,
+                                                fontSize: 28.0,
+                                              ),
+                                            ),
+                                          ),
+
+                                          Divider(
+                                            color: Colors.white,
+                                            height: 8.0,
+                                            thickness: 1.0,
+                                          ),
+
+                                          const SizedBox(
+                                            height: 20.0,
+                                          ),
+
+                                          SizedBox(
+                                            width: double.infinity,
+                                            height: 200.0,
+                                            child: Text(
+                                              snapshot.data!.question,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 22.0),
+                                            ),
+                                          ),
+                                          if (snapshot.data!.songURL != null)
+                                            ValueListenableBuilder(
+                                                valueListenable: isPlaying,
+                                                builder: (context, value, child) =>
+                                                    IconButton(
+                                                        iconSize: 50,
+                                                        icon: (isPlaying.value)
+                                                            ? const Icon(Icons.stop)
+                                                            : Icon(Icons.play_arrow),
+                                                        onPressed: () {
+                                                          isPlaying.value =
+                                                              !isPlaying.value;
+                                                          playPauseAudioNetwork(
+                                                              snapshot.data!.songURL!,
+                                                              isPlaying.value);
+                                                        })),
+
+                                          SizedBox(
+                                            height: 25.0,
+                                          ),
+
+                                          // generate 4 bottons for the answers
+                                          for (int i = 0;
+                                              i < snapshot.data!.answers.length;
+                                              i++)
+                                            Container(
+                                              width: double.infinity,
+                                              margin: EdgeInsets.only(bottom: 18.0),
+                                              child: ElevatedButton(
+                                                style: ButtonStyle(
+                                                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                                                        (Set<MaterialState> states) {
+                                                      if (states.contains(MaterialState.pressed)){
+                                                        if(i==snapshot.data!.correctAnswer){
+                                                          return Colors.greenAccent;
+                                                        } else{
+                                                          return Colors.redAccent;
+                                                        }
+                                                      }
+                                                      return null; // Defer to the widget's default.
+                                                    },
+                                                  ),
+                                                  // shape: StadiumBorder(),
+                                                  // color: Colors.blueAccent,
+                                                  // padding: EdgeInsets.symmetric(
+                                                  //     vertical: 18.0),
+                                                ),
+
+                                                onPressed: () {
+
+                                                  if (_controller!.page?.toInt() == questions!.length - 1) {
+                                                    print("finito");
+                                                    // TODO: pagina result
+                                                    // Navigator.push(
+                                                    //     context,
+                                                    //     MaterialPageRoute(
+                                                    //         builder: (context) => ResultScreen(score)));
+                                                  } else {
+                                                    if(i==snapshot.data!.correctAnswer){
+                                                      //TODO: incrementare score
+                                                    }
+                                                    _controller!.nextPage(
+                                                        duration: Duration(milliseconds: 400),
+                                                        curve: Curves.easeInExpo);
+
+                                                    setState(() {
+                                                      //btnPressed = false;
+                                                    });
+                                                  }
+                                                },
+                                                child: Text(snapshot.data!.answers[i],
+                                                    style: TextStyle(
+                                                        color: Colors.white)),
+                                              ),
+                                            ),
+                                          SizedBox(
+                                            height: 30.0,
+                                          ),
+                                        ]);
+                                  default:
+                                    if (snapshot.hasError)
+                                      return new Text('Error: ${snapshot.error}');
+                                    else
+                                      return new Text('Result: ${snapshot.data}');
+                                }
+                              }),
+                        ],
+                      ),
+                    );
                   }),
-            ],
-          ),
-        ));
+
+        );
   }
 }
