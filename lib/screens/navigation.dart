bc3ab25416ce3150642c3240a5f0d44c5cd0c989
Francesco Corso <92737654+orsoFra@users.noreply.github.com>
2023-01-13 18:50:20 +0100

    new navbar + pulldown to refresh in leaderboard

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:spotiquiz/screens/homepage.dart';
 import 'package:spotiquiz/screens/leaderboard_page.dart';
 import 'package:spotiquiz/screens/userpage.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Navigation extends StatefulWidget {
   const Navigation({super.key});
@@ -25,27 +26,33 @@ class _NavigationState extends State<Navigation> {
       ),
       bottomNavigationBar: SizedBox(
         //height: queryData.size.height * 0.07,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
+        child: GNav(
+          style: GnavStyle.google,
+          rippleColor: const Color.fromARGB(255, 49, 45, 45),
           backgroundColor: Color.fromARGB(255, 30, 25, 25),
-          selectedItemColor: Color.fromARGB(255, 128, 5, 195),
-          unselectedItemColor: Color.fromARGB(255, 49, 45, 45),
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.leaderboard),
-              label: 'Leaderboard',
+          activeColor: Color.fromARGB(255, 128, 5, 195),
+          color: const Color.fromARGB(255, 49, 45, 45),
+          curve: Curves.easeOutExpo,
+          duration: Duration(milliseconds: 300),
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          iconSize: 27,
+          haptic: true,
+          tabs: const <GButton>[
+            GButton(
+              icon: Icons.leaderboard,
+              //text: 'Leaderboard',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+            GButton(
+              icon: Icons.home,
+              //text: 'Home',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profile',
+            GButton(
+              icon: Icons.person,
+              //text: 'Profile',
             ),
           ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
+          selectedIndex: _selectedIndex,
+          onTabChange: _onItemTapped,
         ),
       ),
     );
