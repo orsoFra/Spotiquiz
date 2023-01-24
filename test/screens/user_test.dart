a018c9666470c92b567463559d3fbdea5a4e6772
Francesco Corso <92737654+orsoFra@users.noreply.github.com>
2023-01-24 16:27:20 +0100

    updated userpage + removed welcomepage + updated main page+ USERPAGE TESTS

--- test/screens/user_test.dart
+++ test/screens/user_test.dart
@@ -37,28 +37,115 @@ void main() {
   MockAPI mockAPI = MockAPI();
   MockData mocksD = MockData();
   MockHttpClient mockHttpClient = MockHttpClient();
+  registerFallbackValue(mockHttpClient);
 
   // test the image of profile
-  testWidgets('testUserImageProfile', (tester) async {
-    await tester.runAsync(
+  testWidgets('testUserAvatar', (tester) async {
+    await mockNetworkImagesFor(
       () async {
-        await mockNetworkImagesFor(() async {
-          when(() => mockAPI.getInfoUser(mockHttpClient)).thenAnswer((invocation) => Future.value(
+        await tester.runAsync(() async {
+          when(() => mockAPI.getInfoUser(any())).thenAnswer((invocation) => Future.value(
                 {'imageUrl': '', 'display_name': 'flutterproject', 'email': 'flutterproject@gmail.com', 'product': 'premium'},
               ));
-          when(() => mocksD.getUserData())
-              .thenAnswer((invocation) => Future.value({'name': '', 'numPerfectQuizzes': 'flutterproject', 'numQuizzes': 'flutterproject@gmail.com', 'totalScore': 'premium'}));
+          when(() => mocksD.getUserData()).thenAnswer((invocation) => Future.value({'name': 'flutter', 'numPerfectQuizzes': 1, 'numQuizzes': 1, 'totalScore': 1, 'avgScore': 1}));
           await tester.pumpWidget(buildTestableWidget(UserPage.test(
             dA: mocksD,
             api: mockAPI,
           )));
-          verifyNever((() => mockAPI.getInfoUser(mockHttpClient))).called(0);
-          //verify((() => mocksD.getUserData()).);
-          expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+          verify(() => mockAPI.getInfoUser(any())).called(1);
+          verify(() => mocksD.getUserData()).called(1);
+          await tester.pumpAndSettle();
+          expectLater(find.byType(CircleAvatar), findsWidgets);
+          //expect(find.byType(Text), findsWidgets);
+          /*await tester.pumpAndSettle();
+          */
+          //expect(find.byType(CircularProgressIndicator), findsOneWidget);
 
           // TODO: why hasData is never true?
-          // expectLater(find.image(const NetworkImage('')), findsOneWidget);
+
           // expectLater(find.byType(NetworkImage), findsOneWidget);
+          //await tester.timedDrag(find.byType(CircularProgressIndicator), Offset.fromDirection(0.5), Duration(seconds: 1));
+        });
+      },
+    );
+  });
+
+  testWidgets('test userInfo', (tester) async {
+    await mockNetworkImagesFor(
+      () async {
+        await tester.runAsync(() async {
+          when(() => mockAPI.getInfoUser(any())).thenAnswer((invocation) => Future.value(
+                {'imageUrl': '', 'display_name': 'flutterproject', 'email': 'flutterproject@gmail.com', 'product': 'premium'},
+              ));
+          when(() => mocksD.getUserData()).thenAnswer((invocation) => Future.value({'name': 'flutter', 'numPerfectQuizzes': 1, 'numQuizzes': 1, 'totalScore': 1, 'avgScore': 1}));
+          await tester.pumpWidget(buildTestableWidget(UserPage.test(
+            dA: mocksD,
+            api: mockAPI,
+          )));
+
+          verify(() => mockAPI.getInfoUser(any())).called(1);
+          verify(() => mocksD.getUserData()).called(1);
+          await tester.pumpAndSettle();
+          expect(find.text('flutterproject'), findsWidgets);
+          expect(find.text('flutterproject@gmail.com'), findsWidgets);
+          expect(find.text('premium user '), findsWidgets);
+        });
+      },
+    );
+  });
+
+  testWidgets('test user stats', (tester) async {
+    await mockNetworkImagesFor(
+      () async {
+        await tester.runAsync(() async {
+          when(() => mockAPI.getInfoUser(any())).thenAnswer((invocation) => Future.value(
+                {'imageUrl': '', 'display_name': 'flutterproject', 'email': 'flutterproject@gmail.com', 'product': 'premium'},
+              ));
+          when(() => mocksD.getUserData())
+              .thenAnswer((invocation) => Future.value({'name': 'flutter', 'numPerfectQuizzes': 1, 'numQuizzes': 10, 'totalScore': 100, 'avgScore': 6}));
+          await tester.pumpWidget(buildTestableWidget(UserPage.test(
+            dA: mocksD,
+            api: mockAPI,
+          )));
+
+          verify(() => mockAPI.getInfoUser(any())).called(1);
+          verify(() => mocksD.getUserData()).called(1);
+          await tester.pumpAndSettle();
+          expect(find.text('Completed'), findsWidgets);
+          expect(find.text('Perfect'), findsWidgets);
+          expect(find.text('Score'), findsWidgets);
+          expect(find.text('Average'), findsWidgets);
+          expect(find.text('10'), findsWidgets);
+          expect(find.text('100'), findsWidgets);
+          expect(find.text('1'), findsWidgets);
+          expect(find.text('6'), findsWidgets);
+        });
+      },
+    );
+  });
+
+  testWidgets('test Icons', (tester) async {
+    await mockNetworkImagesFor(
+      () async {
+        await tester.runAsync(() async {
+          when(() => mockAPI.getInfoUser(any())).thenAnswer((invocation) => Future.value(
+                {'imageUrl': '', 'display_name': 'flutterproject', 'email': 'flutterproject@gmail.com', 'product': 'premium'},
+              ));
+          when(() => mocksD.getUserData()).thenAnswer((invocation) => Future.value({'name': 'flutter', 'numPerfectQuizzes': 1, 'numQuizzes': 1, 'totalScore': 1, 'avgScore': 1}));
+          await tester.pumpWidget(buildTestableWidget(UserPage.test(
+            dA: mocksD,
+            api: mockAPI,
+          )));
+
+          verify(() => mockAPI.getInfoUser(any())).called(1);
+          verify(() => mocksD.getUserData()).called(1);
+          await tester.pumpAndSettle();
+          expect(find.byIcon(Icons.done), findsWidgets);
+          expect(find.byIcon(Icons.star_rate), findsWidgets);
+          expect(find.byIcon(Icons.emoji_events), findsWidgets);
+          expect(find.byIcon(Icons.bar_chart), findsWidgets);
+          expect(find.byIcon(Icons.settings), findsWidgets);
         });
       },
     );
