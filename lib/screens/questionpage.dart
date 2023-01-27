2ebad3e42cc505fefb163704eaff7d60efc05205
Francesco Corso <92737654+orsoFra@users.noreply.github.com>
2023-01-27 17:28:45 +0100

    fixed a bug of the player in question page

--- lib/screens/questionpage.dart
+++ lib/screens/questionpage.dart
@@ -88,11 +88,11 @@ class _QuestionPageState extends State<QuestionPage> {
       this.questions = widget.qApi.generateNonListeningQuestions(http.Client(), numQuestions);
 
     if (widget.isTest) {
-      _controller = Get.put(QuestionController(this.stopPlaying, numQuestions));
+      _controller = Get.put(QuestionController(stopMusic, numQuestions));
       timerController = Get.put(TimerController(_controller!));
       timerController.pause();
     } else {
-      _controller = Get.put(QuestionController(this.stopPlaying, numQuestions));
+      _controller = Get.put(QuestionController(stopMusic, numQuestions));
       timerController = Get.put(TimerController(_controller!));
       _controller?.setTimerController(timerController);
       timerController.resetTimerAndStart();
@@ -107,10 +107,15 @@ class _QuestionPageState extends State<QuestionPage> {
     });
   }
 
+  void stopMusic() {
+    assetsAudioPlayer.pause();
+  }
+
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
     _controller?.setContext(context);
+    stopPlaying();
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 25, 20, 20),
       body: PageView.builder(
